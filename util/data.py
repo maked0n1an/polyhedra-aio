0523f38decadbe73a5876d2dfa71e561f39c7152
Alex <amedik733@gmail.com>
2023-08-15 00:49:26 +0300

    Updated data.py

--- util/data.py
+++ util/data.py
@@ -50,34 +50,34 @@ DATA = {
 
 ''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ За этой линией не менять ! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 
-legendary_panda_config = [
-    # [Chain.BSC, Chain.POLYGON],
-    # [Chain.BSC, Chain.CORE],
-    # [Chain.BSC, Chain.CELO],
+bridge_pandra_config = [
+    # # [Chain.BSC, Chain.POLYGON],
+    # # [Chain.BSC, Chain.CORE],
+    # # [Chain.BSC, Chain.CELO],
     # [Chain.BSC, Chain.COMBO_TESTNET],
-    [Chain.BSC, Chain.OP_BNB],
-    # [Chain.BSC, Chain.MANTLE],
+    # [Chain.BSC, Chain.OP_BNB],
     # ########################
     # [Chain.POLYGON, Chain.BSC],
-    # [Chain.POLYGON, Chain.CORE],
+    # # [Chain.POLYGON, Chain.CORE],
     # [Chain.POLYGON, Chain.CELO],
-    # [Chain.POLYGON, Chain.COMBO_TESTNET],
+    [Chain.POLYGON, Chain.COMBO_TESTNET],
     # [Chain.POLYGON, Chain.OP_BNB],
-    # [Chain.POLYGON, Chain.MANTLE],
     # ########################
     # [Chain.CORE, Chain.BSC],
-    # [Chain.CORE, Chain.POLYGON],
-    # [Chain.CORE, Chain.POLYGON, Chain.CELO],
+    # # [Chain.CORE, Chain.POLYGON],
+    # # [Chain.CORE, Chain.POLYGON, Chain.CELO],
     # [Chain.CORE, Chain.COMBO_TESTNET],
     # [Chain.CORE, Chain.OP_BNB],
-    # [Chain.CORE],
     # ######################### 
     # [Chain.CELO, Chain.BSC],   
-    # [Chain.CELO, Chain.POLYGON],
+    # # [Chain.CELO, Chain.POLYGON],
     # [Chain.CELO, Chain.POLYGON, Chain.CORE],    
     # [Chain.CELO, Chain.COMBO_TESTNET],
     # [Chain.CELO, Chain.OP_BNB],
-    # [Chain.CELO],
+]
+
+mint_pandra_config = [
+    Chain.CORE, Chain.CELO, Chain.POLYGON, Chain.BSC, Chain.OP_BNB 
 ]
 
 mailer_abi =                    '[{"inputs":[{"internalType":"address","name":"_zkBridgeEntrypoint","type":"address"},{"internalType":"address","name":"_lzEndpoint","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint64","name":"sequence","type":"uint64"},{"indexed":true,"internalType":"uint32","name":"dstChainId","type":"uint32"},{"indexed":true,"internalType":"address","name":"dstAddress","type":"address"},{"indexed":false,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"address","name":"recipient","type":"address"},{"indexed":false,"internalType":"string","name":"message","type":"string"}],"name":"LzMessageSend","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint64","name":"sequence","type":"uint64"},{"indexed":true,"internalType":"uint32","name":"dstChainId","type":"uint32"},{"indexed":true,"internalType":"address","name":"dstAddress","type":"address"},{"indexed":false,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"address","name":"recipient","type":"address"},{"indexed":false,"internalType":"string","name":"message","type":"string"}],"name":"MessageSend","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint16","name":"chainId","type":"uint16"},{"indexed":false,"internalType":"uint256","name":"fee","type":"uint256"}],"name":"NewFee","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"},{"indexed":false,"internalType":"bool","name":"zkBridgePaused","type":"bool"},{"indexed":false,"internalType":"bool","name":"layerZeroPaused","type":"bool"}],"name":"PauseSendAction","type":"event"},{"inputs":[],"name":"claimFees","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint16","name":"_dstChainId","type":"uint16"},{"internalType":"address","name":"_recipient","type":"address"},{"internalType":"string","name":"_message","type":"string"}],"name":"estimateLzFee","outputs":[{"internalType":"uint256","name":"nativeFee","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint16","name":"","type":"uint16"}],"name":"fees","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint16","name":"_srcChainId","type":"uint16"},{"internalType":"bytes","name":"_srcAddress","type":"bytes"}],"name":"forceResumeReceive","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint16","name":"_version","type":"uint16"},{"internalType":"uint16","name":"_dstChainId","type":"uint16"},{"internalType":"uint256","name":"_configType","type":"uint256"}],"name":"getConfig","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getSendVersion","outputs":[{"internalType":"uint16","name":"","type":"uint16"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"layerZeroPaused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lzEndpoint","outputs":[{"internalType":"contract ILayerZeroEndpoint","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint16","name":"lzChainId","type":"uint16"},{"internalType":"address","name":"lzDstAddress","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"string","name":"message","type":"string"}],"name":"lzSendMessage","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"maxLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bool","name":"zkBridgePaused_","type":"bool"},{"internalType":"bool","name":"layerZeroPaused_","type":"bool"}],"name":"pause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint16","name":"dstChainId","type":"uint16"},{"internalType":"address","name":"dstAddress","type":"address"},{"internalType":"uint16","name":"lzChainId","type":"uint16"},{"internalType":"address","name":"lzDstAddress","type":"address"},{"internalType":"uint256","name":"nativeFee","type":"uint256"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"string","name":"message","type":"string"}],"name":"sendMessage","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint16","name":"_version","type":"uint16"},{"internalType":"uint16","name":"_dstChainId","type":"uint16"},{"internalType":"uint256","name":"_configType","type":"uint256"},{"internalType":"bytes","name":"_config","type":"bytes"}],"name":"setConfig","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint16","name":"_dstChainId","type":"uint16"},{"internalType":"uint256","name":"_fee","type":"uint256"}],"name":"setFee","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_maxLength","type":"uint256"}],"name":"setMsgLength","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint16","name":"_version","type":"uint16"}],"name":"setReceiveVersion","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint16","name":"_version","type":"uint16"}],"name":"setSendVersion","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"zkBridgeEntrypoint","outputs":[{"internalType":"contract IZKBridgeEntrypoint","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"zkBridgePaused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint16","name":"dstChainId","type":"uint16"},{"internalType":"address","name":"dstAddress","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"string","name":"message","type":"string"}],"name":"zkSendMessage","outputs":[],"stateMutability":"payable","type":"function"}]'
@@ -186,12 +186,13 @@ nfts_addresses = {
         Chain.BSC:              '0x87a218Ae43C136B3148A45EA1A282517794002c8',
         Chain.POLYGON:          '0x141A1fb33683C304DA7C3fe6fC6a49B5C0c2dC42',
         Chain.CORE:             '0x36e5121618c0Af89E81AcD33B6b8F5CF5cDD961a',
-        Chain.CELO:             '0xb404e5233aB7E426213998C025f05EaBaBD41Da6'
+        Chain.CELO:             '0xb404e5233aB7E426213998C025f05EaBaBD41Da6',
+        Chain.OP_BNB:           '0x95A44287A6D208FA723A899D971d3976cA985ba6'
     },
 
     'Bridged Pandra': {
-        Chain.CELO + '->' + Chain.POLYGON:          '0x9d03ab31c0e71d909c68d3f6bb7483577ba8dcd7', # Pandra: Ecoguardian (Celo) in Polygon
-        Chain.CORE + '->' + Chain.POLYGON:          '0x20fb7ec336958ea887a288ce49999d7d7c08529c', # Pandra: MelodyMaven (Core) in Polygon
+        Chain.CELO:          '0x9d03ab31c0e71d909c68d3f6bb7483577ba8dcd7', # Pandra: Ecoguardian (Celo) in Polygon
+        Chain.CORE:          '0x20fb7ec336958ea887a288ce49999d7d7c08529c', # Pandra: MelodyMaven (Core) in Polygon
     }
 }
 
